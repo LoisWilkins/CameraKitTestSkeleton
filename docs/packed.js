958e0a0c138b5da3314efdd3f8953ed6c7d18998
LoisWilkins <lois@wilkins-avenue.com>
2023-10-13 13:39:15 +0200

    new test with sapin

--- docs/packed.js
+++ docs/packed.js
@@ -26020,25 +26020,33 @@ console.info(`SDK: ${environment_namespaceObject.l} \
 
 //# sourceMappingURL=index.js.map
 ;// CONCATENATED MODULE: ./src/main.js
-
+ //module ajouter après le npm install
 
 (async function(){
-    var cameraKit = await bootstrapCameraKit({apiToken: 'eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNjk3MTIzODM1LCJzdWIiOiJlOTM0MjFjZC0yODZiLTRiMzktOWU1YS1kODRlZWZhMzRkNWZ-U1RBR0lOR35jYWNhZWM2MC1lYTgwLTQ1MTktYmEzOC0yMDAyYjc0YzBhMmMifQ.RXrD2bvTHFVsfixBQtqdYjuTre0Uj5W-jWDIbF-qL1A'})
-    const session= await cameraKit.createSession()
-    document.getElementById('canvas').replaceWith(session.output.live)
+    //token de la lens
 
-    const {lenses}= await cameraKit.lensRepository.loadLensGroups(['26913f30-2f98-44c5-87be-44208fb30fcb'])
+    var token1='eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNjk3MTIzODM1LCJzdWIiOiJlOTM0MjFjZC0yODZiLTRiMzktOWU1YS1kODRlZWZhMzRkNWZ-U1RBR0lOR35jYWNhZWM2MC1lYTgwLTQ1MTktYmEzOC0yMDAyYjc0YzBhMmMifQ.RXrD2bvTHFVsfixBQtqdYjuTre0Uj5W-jWDIbF-qL1A'
+    var token2='eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNjk3MTk2NTYxLCJzdWIiOiJlYWU4N2ZlMi00ZDZhLTQ4YzItOWYxZi0yM2FhYjUxZmU3NzB-U1RBR0lOR344ODUzMmQzZC02YjhmLTQ4ZTEtOTcxZS01NjJlMTI3MmNhMTIifQ.tESA8QNw4YLTPLLWvv-v3BwMhPYjxZiT041QFapoJcs'
+    var token=token2
+    var cameraKit = await bootstrapCameraKit({apiToken: token}) 
+    const session= await cameraKit.createSession()
+    document.getElementById('canvas').replaceWith(session.output.live) //Le contenu cameraKit se met dans un canvas
+
+    var key1='26913f30-2f98-44c5-87be-44208fb30fcb'
+    var key2='d0371f69-7202-422a-a0d2-bf54b1adf8ed'
+    var groupID=key2
+    const {lenses}= await cameraKit.lensRepository.loadLensGroups([groupID])
 
     session.applyLens(lenses[0])
     let mediaStream= await navigator.mediaDevices.getUserMedia({
-        video: { facingMode: 'environment' }
+        video: { facingMode: 'environment' } //pour activer la caméra arrrière
     
     });
 
     const source =createMediaStreamSource(mediaStream,{
-        // transform: Transform2D.MirrorX,
-        // cameraType: 'front'
-        cameraType: 'back'
+        // transform: Transform2D.MirrorX, //pas necessaire pour la demo
+        // cameraType: 'front' //pour activer la caméra avant
+        cameraType: 'back' //pour activer la caméra arrrière
     })
 
     await session.setSource(source)
